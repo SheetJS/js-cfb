8bb0c56acd34d504cd2ae16160180427ac86b706
SheetJS <dev@sheetjs.com>
2018-04-21 10:16:31 -0400

    flow switch to module.exports

--- misc/flowdeps.js
+++ misc/flowdeps.js
@@ -1,10 +1,12 @@
 /*::
 
-declare module 'cfb' { declare var exports:CFBModule; };
-declare module '../' { declare var exports:CFBModule; };
-declare module './' { declare var exports:CFBModule; };
+declare module 'cfb' { declare module.exports:CFBModule; };
+declare module '../' { declare module.exports:CFBModule; };
+declare module './' { declare module.exports:CFBModule; };
 
-declare module 'commander' { declare var exports:any; };
-declare module 'printj' { declare var exports:any; };
-declare module 'crc-32' { declare var exports: any; };
+declare module 'commander' { declare module.exports:any; };
+declare module 'printj' {
+	declare var sprintf:(fmt:string, ...args:any)=>string;
+};
+declare module 'crc-32' { declare module.exports: any; };
 */
